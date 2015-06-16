2c6145247823b5e860bf25951511b9a9e71f578f
AumitLeon <aumitleon1@gmail.com>
2015-06-16 13:30:51 -0400

    Addition of a function (fillRandomly) that randomly fills the board with numbers 1-9. Also, restructure board.

--- SudokuBoard.cpp
+++ SudokuBoard.cpp
@@ -1,16 +1,18 @@
 #include "SudokuBoard.h"
 #include <iostream>
+#include <ctime>
+#include <cstdlib>
 using namespace std;
 
 SudokuBoard::SudokuBoard()
 {
-    for (int i = 0; i < height; i++)
+   /* for (int i = 0; i < height; i++)
     {
         for (int m = 0; m < width; m++)
         {
             sudoku_board[i][m] = (i+1)*(m+1);
         }
-    }
+    }*/
 }
 
 SudokuBoard::~SudokuBoard()
@@ -20,14 +22,36 @@ SudokuBoard::~SudokuBoard()
 
 void SudokuBoard::displayBoard()
 {
+    fillRandomly();
+    cout << "----------------------------------------------" << endl;
     for (int f = 0; f < 9; f++)
     {
         for (int h = 0; h < 9; h++)
         {
-            if (sudoku_board[f][h] <= 9)
-                cout << sudoku_board[f][h] << "  | ";
-            else cout << sudoku_board[f][h] << " | ";
+            if (h == 2 || h == 5)
+                cout << " " << sudoku_board[f][h] << " || ";
+            else cout << " " << sudoku_board[f][h] << " | ";
+            //else cout << sudoku_board[f][h] << " | ";
         }
         cout << endl;
+        if (f == 2 || f == 5)
+        {
+            cout << "----------------------------------------------" << endl;
+            cout << "----------------------------------------------" << endl;
+        }
+        else cout << "----------------------------------------------" << endl;
+    }
+}
+
+void SudokuBoard::fillRandomly()
+{
+    srand(time(0));
+
+    for (int i = 0; i < height; i++)
+    {
+        for (int m = 0; m < width; m++)
+        {
+            sudoku_board[i][m] = 1+rand()%9;
+        }
     }
 }
