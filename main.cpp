80d2f30dfe3c9a853953f130762c387966afbec9
AumitLeon <aumitleon1@gmail.com>
2015-07-08 14:43:59 -0400

    New strategy for developing board-- reads input from file.

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
+#include <fstream>
 #include "SudokuBoard.h"
+#include<string>
 using namespace std;
 
 int main()
@@ -11,6 +13,25 @@ int main()
     cout << endl;
     cout << endl;
 
-    SudokuBoard board;
-    board.displayBoard();
+    fstream board__ ("board.txt");
+    string inputs;
+    int i = 0;
+    SudokuBoard sudoku_board;
+
+    while(getline(board__, inputs))
+    {
+        for (int m = 0; m < sudoku_board.getWidth(); m++)
+            {
+                if (inputs[m] != '0')
+                {
+                    sudoku_board.input(i, m, inputs[m]);
+                }
+                else
+                {
+                    sudoku_board.input(i, m, ' ');
+                }
+            }
+        i++;
+    }
+sudoku_board.displayBoard();
 }
